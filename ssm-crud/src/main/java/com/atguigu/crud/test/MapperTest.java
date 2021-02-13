dd1cf238aa5de199052a6425a5a794d24f5311eb
Yanlan <Yanlan@Yanlan-PC>
2021-02-13 17:06:15 -0500

    init commit, including: configuration of pom.xml, applicationContex.xml for spring, web.xml for springMVC, mybatis-config for mybatis, mbg.xml for mybatis generator

---
+++ ssm-crud/src/main/java/com/atguigu/crud/test/MapperTest.java
@@ -0,0 +1,57 @@
+package com.atguigu.crud.test;
+
+import java.util.UUID;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.atguigu.crud.bean.Department;
+import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.dao.DepartmentMapper;
+import com.atguigu.crud.dao.EmployeeMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContex.xml" })
+public class MapperTest {
+
+	@Autowired
+	DepartmentMapper departmentMapper;
+
+	@Autowired
+	EmployeeMapper employeeMapper;
+
+	@Autowired
+	SqlSession sqlSession;
+
+	@Test
+	public void testCRUD() {
+
+		// Insert records to table dept
+		departmentMapper.insertSelective(new Department(null, "Developement Department"));
+		departmentMapper.insertSelective(new Department(null, "Test Department"));
+
+		System.out.println(departmentMapper);
+	}
+
+	@Test
+	public void testCRUD1() {
+		// Insert records to table emp
+		employeeMapper.insertSelective(new Employee(null, "Jerry", "M", "jerry@hotmail.com", 1));
+		employeeMapper.insertSelective(new Employee(null, "Tom", "M", "tom@hotmail.com", 2));
+	}
+
+	@Test
+	public void testCRUDBatch() {
+		EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+		for (int i = 0; i < 10; i++) {
+			String uid = UUID.randomUUID().toString().substring(0, 5) + i;
+			mapper.insertSelective(new Employee(null, uid, "M", uid + "@hotmail.com", 1));
+		}
+		System.out.println("Success");
+	}
+
+}
---
+++ ssm-crud/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script type="text/javascript" src="static/js/jquery-1.8.2.js"></script>
+<link rel="stylesheet"
+	href="static/bootstrap-3.4.1/css/bootstrap.min.css">
+<script src="static/bootstrap-3.4.1/js/bootstrap.min.js"></script>
+<title>Insert title here</title>
+</head>
+<body>
+	<button class="btn btn-success">submit</button>
+</body>
+</html>
